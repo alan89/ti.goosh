3aebaf51b4cac1fbb974d825c305417ad4b764b9
alan89 <alan.escobar.martinez@gmail.com>
2016-11-15 16:05:20 -0600

    Add support for 6.0.0.GA, version bump, V8

---
+++ android/build/generated/obj/local/armeabi-v7a/objs/ti.goosh/ti.goosh.TiGooshModule.o.d
@@ -0,0 +1,160 @@
+/var/folders/j7/8n1k11f12qb1rfgdvrxxy__80000gn/T//VincoAlan/ti.goosh-generated/obj/local/armeabi-v7a/objs/ti.goosh/ti.goosh.TiGooshModule.o: \
+ jni/ti.goosh.TiGooshModule.cpp jni/ti.goosh.TiGooshModule.h \
+ /Users/VincoAlan/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/android/native/include/Proxy.h \
+ /Users/VincoAlan/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/android/native/include/v8.h \
+ /Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/../../android/support/include/stdint.h \
+ /Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/../../android/support/include/stdio.h \
+ /Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/../../android/support/include/wchar.h \
+ /Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/../../android/support/include/wctype.h \
+ /Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/../../android/support/include/xlocale.h \
+ /Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/libcxx/include/utility \
+ /Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/libcxx/include/__config \
+ /Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/libcxx/include/__tuple \
+ /Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/libcxx/include/cstddef \
+ /Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/libcxx/include/type_traits \
+ /Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/libcxx/include/vector \
+ /Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/libcxx/include/__bit_reference \
+ /Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/libcxx/include/algorithm \
+ /Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/libcxx/include/initializer_list \
+ /Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/libcxx/include/cstring \
+ /Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/../../android/support/include/string.h \
+ /Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/libcxx/include/memory \
+ /Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/libcxx/include/typeinfo \
+ /Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/libcxx/include/exception \
+ /Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/libcxx/include/cstdint \
+ /Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/libcxx/include/new \
+ /Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/libcxx/include/limits \
+ /Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/libcxx/include/iterator \
+ /Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/libcxx/include/__functional_base \
+ /Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/libcxx/include/iosfwd \
+ /Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/libcxx/include/tuple \
+ /Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/libcxx/include/cassert \
+ /Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/libcxx/include/atomic \
+ /Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/libcxx/include/climits \
+ /Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/libcxx/include/stdexcept \
+ /Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/libcxx/include/__split_buffer \
+ /Users/VincoAlan/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/android/native/include/v8-version.h \
+ /Users/VincoAlan/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/android/native/include/v8config.h \
+ /Users/VincoAlan/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/android/native/include/JavaObject.h \
+ /Users/VincoAlan/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/android/native/include/EventEmitter.h \
+ /Users/VincoAlan/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/android/native/include/NativeObject.h \
+ /Users/VincoAlan/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/android/native/include/AndroidUtil.h \
+ /Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/../../android/support/include/stdlib.h \
+ /Users/VincoAlan/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/android/native/include/EventEmitter.h \
+ /Users/VincoAlan/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/android/native/include/JNIUtil.h \
+ /Users/VincoAlan/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/android/native/include/AndroidUtil.h \
+ /Users/VincoAlan/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/android/native/include/JSException.h \
+ /Users/VincoAlan/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/android/native/include/ProxyFactory.h \
+ /Users/VincoAlan/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/android/native/include/TypeConverter.h \
+ /Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/libcxx/include/map \
+ /Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/libcxx/include/__tree \
+ /Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/libcxx/include/functional \
+ /Users/VincoAlan/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/android/native/include/V8Util.h \
+ /Users/VincoAlan/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/android/native/include/org.appcelerator.kroll.KrollModule.h \
+ /Users/VincoAlan/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/android/native/include/Proxy.h
+
+jni/ti.goosh.TiGooshModule.h:
+
+/Users/VincoAlan/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/android/native/include/Proxy.h:
+
+/Users/VincoAlan/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/android/native/include/v8.h:
+
+/Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/../../android/support/include/stdint.h:
+
+/Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/../../android/support/include/stdio.h:
+
+/Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/../../android/support/include/wchar.h:
+
+/Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/../../android/support/include/wctype.h:
+
+/Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/../../android/support/include/xlocale.h:
+
+/Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/libcxx/include/utility:
+
+/Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/libcxx/include/__config:
+
+/Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/libcxx/include/__tuple:
+
+/Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/libcxx/include/cstddef:
+
+/Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/libcxx/include/type_traits:
+
+/Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/libcxx/include/vector:
+
+/Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/libcxx/include/__bit_reference:
+
+/Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/libcxx/include/algorithm:
+
+/Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/libcxx/include/initializer_list:
+
+/Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/libcxx/include/cstring:
+
+/Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/../../android/support/include/string.h:
+
+/Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/libcxx/include/memory:
+
+/Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/libcxx/include/typeinfo:
+
+/Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/libcxx/include/exception:
+
+/Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/libcxx/include/cstdint:
+
+/Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/libcxx/include/new:
+
+/Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/libcxx/include/limits:
+
+/Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/libcxx/include/iterator:
+
+/Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/libcxx/include/__functional_base:
+
+/Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/libcxx/include/iosfwd:
+
+/Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/libcxx/include/tuple:
+
+/Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/libcxx/include/cassert:
+
+/Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/libcxx/include/atomic:
+
+/Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/libcxx/include/climits:
+
+/Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/libcxx/include/stdexcept:
+
+/Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/libcxx/include/__split_buffer:
+
+/Users/VincoAlan/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/android/native/include/v8-version.h:
+
+/Users/VincoAlan/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/android/native/include/v8config.h:
+
+/Users/VincoAlan/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/android/native/include/JavaObject.h:
+
+/Users/VincoAlan/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/android/native/include/EventEmitter.h:
+
+/Users/VincoAlan/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/android/native/include/NativeObject.h:
+
+/Users/VincoAlan/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/android/native/include/AndroidUtil.h:
+
+/Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/../../android/support/include/stdlib.h:
+
+/Users/VincoAlan/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/android/native/include/EventEmitter.h:
+
+/Users/VincoAlan/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/android/native/include/JNIUtil.h:
+
+/Users/VincoAlan/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/android/native/include/AndroidUtil.h:
+
+/Users/VincoAlan/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/android/native/include/JSException.h:
+
+/Users/VincoAlan/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/android/native/include/ProxyFactory.h:
+
+/Users/VincoAlan/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/android/native/include/TypeConverter.h:
+
+/Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/libcxx/include/map:
+
+/Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/libcxx/include/__tree:
+
+/Users/VincoAlan/Documents/Android_SDK/android-ndk-r11c/sources/cxx-stl/llvm-libc++/libcxx/include/functional:
+
+/Users/VincoAlan/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/android/native/include/V8Util.h:
+
+/Users/VincoAlan/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/android/native/include/org.appcelerator.kroll.KrollModule.h:
+
+/Users/VincoAlan/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/android/native/include/Proxy.h:
